d6e93464f01992fb2c016dc1738efd0c5bd6e4ae
Collin Garvey <collin.garvey@gmail.com>
2020-03-16 00:59:11 -0400

    Adds basic car machine

---
+++ src/carMachine.ts
@@ -0,0 +1,37 @@
+import { Machine, interpret } from "xstate";
+
+interface CarStateSchema {
+  states: {
+    idle: {};
+    ascend: {};
+    descend: {};
+  };
+}
+
+interface CarContext {
+  currentFloor: number;
+}
+
+type CarEvent = { type: "IDLE" } | { type: "ASCEND" } | { type: "DESCEND" };
+
+const carMachine = Machine<CarContext, CarStateSchema, CarEvent>({
+  key: "car",
+  initial: "idle",
+  context: {
+    currentFloor: 0
+  },
+  states: {
+    idle: {
+      on: {
+        ASCEND: "ascend",
+        DESCEND: "descend"
+      }
+    },
+    ascend: { on: { DESCEND: "descend" } },
+    descend: { on: { ASCEND: "ascend", IDLE: "idle" } }
+  }
+});
+
+const taco = interpret(carMachine)
+  .onTransition(state => console.log(state.value))
+  .start();
